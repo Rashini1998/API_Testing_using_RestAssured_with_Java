84c4cf45b9dd47aa91ee3820287799a18188c20a
Rashini1998 <gamalathrs.19@uom.lk>
2023-11-15 20:26:18 +0530

    New API tests-2

---
+++ src/test/java/StatusCodes.java
@@ -0,0 +1,12 @@
+public class StatusCodes {
+
+    // Define status codes as static variables
+    public static final int STATUS_CODE_SUCCESS=200;
+    public static final int STATUS_CODE_CREATED=201;
+    public static final int STATUS_CODE_BAD_REQUEST=400;
+    public static final int STATUS_CODE_UNAUTHORIZED=401;
+    public static final int STATUS_CODE_FORBIDDEN=403;
+    public static final int STATUS_CODE_NOT_FOUND=404;
+    public static final int STATUS_CODE_ALREADY_EXISTS=208;
+
+}
